548ea15e2089ea0802444f68c9ccde621e7f4e67
mq1n <koraydemirci86@gmail.com>
2020-06-09 18:37:03 +0300

    remove GAIDEN defination

--- source/client/eterXClient/Packet.h
+++ source/client/eterXClient/Packet.h
@@ -157,13 +157,8 @@ enum
 	HEADER_GC_PLAYER_POINT_CHANGE				= 17,
 	HEADER_GC_CHANGE_SPEED						= 18,
 	HEADER_GC_CHARACTER_UPDATE                  = 19,
-#if defined(GAIDEN)
-	HEADER_GC_ITEM_DEL							= 20, // 아이템 창에 추가
-	HEADER_GC_ITEM_SET							= 21, // 아이템 창에 추가
-#else
 	HEADER_GC_ITEM_SET							= 20, // 아이템 창에 추가
 	HEADER_GC_ITEM_SET2							= 21, // 아이템 창에 추가
-#endif
 	HEADER_GC_ITEM_USE							= 22, // 아이템 사용 (주위 사람들에게 보여주기 위해)
 	HEADER_GC_ITEM_DROP							= 23, // 아이템 버리기
 	HEADER_GC_ITEM_UPDATE						= 25, // 아이템 수치 업데이트
@@ -249,15 +244,9 @@ enum
 	HEADER_GC_WALK_MODE							= 111, 
 	HEADER_GC_CHANGE_SKILL_GROUP				= 112,
 
-#if defined(GAIDEN)
-	HEADER_GC_MAIN_CHARACTER					= 113,
-	HEADER_GC_MAIN_CHARACTER3_BGM				= 137,
-	HEADER_GC_MAIN_CHARACTER4_BGM_VOL			= 138,
-#else
 	// SUPPORT_BGM
 	HEADER_GC_MAIN_CHARACTER2_EMPIRE			= 113,
 	// END_OF_SUPPORT_BGM
-#endif
 
     HEADER_GC_SEPCIAL_EFFECT                    = 114,
 	HEADER_GC_NPC_POSITION						= 115,
@@ -293,17 +282,6 @@ enum
 	// END_OF_SUPPORT_BGM
 
     HEADER_GC_AUTH_SUCCESS                      = 150,
-
-#if defined(GAIDEN)
-	HEADER_GC_ONTIME							= 204,
-	HEADER_GC_RESET_ONTIME						= 205,
-
-	// AUTOBAN
-	HEADER_GC_AUTOBAN_QUIZ						= 206,
-	// END_OF_AUTOBAN
-
-#else
-#endif
 
 	HEADER_GC_SPECIFIC_EFFECT					= 208,
 	HEADER_GC_DRAGON_SOUL_REFINE						= 209,
@@ -1614,44 +1592,6 @@ typedef struct packet_motion
 	WORD		motion;
 } TPacketGCMotion;
 
-#if defined(GAIDEN)
-struct TPacketGCItemDelDeprecate
-{
-    TPacketGCItemDelDeprecate() :
-        header(HEADER_GC_ITEM_DEL),
-        pos(0),
-        vnum(0),
-        count(0)
-    {
-        memset(&alSockets, 0, sizeof(alSockets));
-        memset(&aAttr, 0, sizeof(aAttr));
-    }
-
-    BYTE    header;
-    BYTE    pos;
-    DWORD   vnum;
-    BYTE    count;
-    long    alSockets[ITEM_SOCKET_SLOT_MAX_NUM];
-    TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_SLOT_MAX_NUM];
-};
-
-typedef struct packet_set_item
-{
-	BYTE		header;
-	BYTE		pos;
-	DWORD		vnum;
-	BYTE		count;
-	DWORD		flags;	// 플래그 추가
-	long		alSockets[ITEM_SOCKET_SLOT_MAX_NUM];
-    TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_SLOT_MAX_NUM];
-} TPacketGCItemSet;
-
-typedef struct packet_item_del
-{
-    BYTE        header;
-    BYTE        pos;
-} TPacketGCItemDel;
-#else
 typedef struct packet_set_item
 {
 	BYTE		header;
@@ -1674,7 +1614,6 @@ typedef struct packet_set_item2
 	long		alSockets[ITEM_SOCKET_SLOT_MAX_NUM];
     TPlayerItemAttribute aAttr[ITEM_ATTRIBUTE_SLOT_MAX_NUM];
 } TPacketGCItemSet2;
-#endif
 
 typedef struct packet_item_del
 {
@@ -2513,16 +2452,6 @@ typedef struct packet_state
 	DWORD			dwTime;
 	TPixelPosition	kPPos;
 } TPacketCCState;
-
-// AUTOBAN
-typedef struct packet_autoban_quiz
-{
-    BYTE bHeader;
-	BYTE bDuration;
-    BYTE bCaptcha[64*32];
-    char szQuiz[256];
-} TPacketGCAutoBanQuiz;
-// END_OF_AUTOBAN
 
 #ifdef _IMPROVED_PACKET_ENCRYPTION_
 struct TPacketKeyAgreement
